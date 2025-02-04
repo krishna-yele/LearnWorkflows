1546e073692a9e973825a9aed47f150178671c24
krishna-yele <krishnayele@outlook.com>
2025-02-04 14:18:25 +0530

    Update log_the_message.ps1

--- PSscripts/log_the_message.ps1
+++ PSscripts/log_the_message.ps1
@@ -2,7 +2,7 @@ echo "::debug::This is a debug message"
 echo "::warning::This is a warning message"
 echo "::error::This is an error message"
 echo "## This is a log message to the summary, by Krishna" >> $GITHUB_STEP_SUMMARY
-echo "::set-output name=message::This is an output message" 
+#echo "::set-output name=message::This is an output message" 
 $result = "Success"
 $details = "All steps completed successfully."
 
