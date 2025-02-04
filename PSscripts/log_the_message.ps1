7e84eb6ea7987193ae4d77d70fe1760859430829
krishna-yele <krishnayele@outlook.com>
2025-02-04 14:05:24 +0530

    Create log_the_message.ps1

---
+++ PSscripts/log_the_message.ps1
@@ -0,0 +1,5 @@
+echo "::debug::This is a debug message"
+echo "::warning::This is a warning message"
+echo "::error::This is an error message"
+echo "## This is a log message to the summary" >> $GITHUB_STEP_SUMMARY
+echo "::set-output name=message::This is an output message" 
