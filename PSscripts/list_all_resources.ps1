f11a17d88974af29ec7efa25ed5a9081be15054b
krishna-yele <krishnayele@outlook.com>
2025-02-10 16:10:14 +0530

    Update list_all_resources.ps1

--- PSscripts/list_all_resources.ps1
+++ PSscripts/list_all_resources.ps1
@@ -1,5 +1,7 @@
 $resourceGroupName = "dlpm-kry025-rg" 
-echo $resourceGroupName 
+echo $resourceGroupName
+
+Get-AzADGroup -ObjectId "c80687b4-2468-436b-8db5-d4b531e95024"
 # List resources in the specified resource group 
 $resources = Get-AzResource -ResourceGroupName $resourceGroupName 
 $resources | Format-Table 
