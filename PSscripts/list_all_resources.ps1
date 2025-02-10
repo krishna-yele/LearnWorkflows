30c24221dd85c8c2ba2f320784b426346b8921ca
krishna-yele <krishnayele@outlook.com>
2025-02-10 15:35:19 +0530

    Update list_all_resources.ps1

--- PSscripts/list_all_resources.ps1
+++ PSscripts/list_all_resources.ps1
@@ -1,7 +1,7 @@
-$resourceGroupName = "dlpm-kry025-rg" `    
-echo $resourceGroupName `
-# List resources in the specified resource group `
-$resources = Get-AzResource -ResourceGroupName $resourceGroupName `
-$resources | Format-Table `    
-# Log resource list to summary `
+$resourceGroupName = "dlpm-kry025-rg" 
+echo $resourceGroupName 
+# List resources in the specified resource group 
+$resources = Get-AzResource -ResourceGroupName $resourceGroupName 
+$resources | Format-Table 
+# Log resource list to summary 
 $resources | Format-Table | Out-String | ForEach-Object { Write-Output "## $_" } >> $env:GITHUB_STEP_SUMMARY
