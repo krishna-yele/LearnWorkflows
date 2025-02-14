3f2a82809dea1ecb01ac29f7b1eed8a6796fc369
krishna-yele <krishnayele@outlook.com>
2025-02-14 16:37:28 +0530

    Create Get_LookupTables.ps1

---
+++ PSscripts/Get_LookupTables.ps1
@@ -0,0 +1,15 @@
+function Get-LookupTable {
+    param (
+        [string]$Table,
+        [string]$Query
+    )
+    $csvFile = Import-csv -Path "lookups\$Table.csv"
+    #Write-Host $csvFile 
+
+    if (!$csvFile) {
+        write-host "Could not find the csv file"
+        throw "Could not retrieve / find the csv file"
+    } 
+    return $csvFile
+}
+
